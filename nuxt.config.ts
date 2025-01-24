d43262dc873e0d9eb97caada38e59a308d5d4a92
semajame <jamesanquillano@gmail.com>
2025-01-24 22:51:55 +0800

    first commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-11-01",
+  compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 
   modules: [
-    "@nuxtjs/tailwindcss",
-    "@nuxtjs/google-fonts",
-    "@nuxt/icon",
-    "@nuxt/image",
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/google-fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxtjs/supabase',
   ],
 
   googleFonts: {
@@ -19,6 +20,6 @@ export default defineNuxtConfig({
 
     prefetch: true, // Preload fonts during the build
     preconnect: true, // Preconnect to Google's font servers
-    display: "swap", // Ensure text remains visible during font loading
+    display: 'swap', // Ensure text remains visible during font loading
   },
-});
+})
